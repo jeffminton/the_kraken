10b3eb2e2e514579fb08b9384dee59faa0bb12c0
Thea Steele <theasteele@gmail.com>
2011-08-01 10:33:07 -0400

    in progress game

---
+++ FishActivity.java
@@ -0,0 +1,80 @@
+package com.android.fish;
+
+import org.cocos2d.layers.CCScene;
+import org.cocos2d.nodes.CCDirector;
+import org.cocos2d.opengl.CCGLSurfaceView;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class FishActivity extends Activity {
+	protected CCGLSurfaceView _glSurfaceView;
+	
+	
+    /** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+	    super.onCreate(savedInstanceState);
+	 
+	    requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+	 
+	    _glSurfaceView = new CCGLSurfaceView(this);
+	 
+	    setContentView(_glSurfaceView);
+	    
+	    Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+	    int width = display.getWidth(); 
+	    int height = display.getHeight();
+	    
+	    if (height > width){
+	    	setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+	    }
+	}
+	
+	@Override
+	public void onStart()
+	{
+	    super.onStart();
+	 
+	    CCDirector.sharedDirector().attachInView(_glSurfaceView);
+	 
+	    CCDirector.sharedDirector().setDisplayFPS(true);
+	 
+	    CCDirector.sharedDirector().setAnimationInterval(1.0f / 60.0f);
+	    
+	    CCScene scene = FishGameLayer.scene();
+	    CCDirector.sharedDirector().runWithScene(scene);
+	}
+	
+	@Override
+	public void onPause()
+	{
+	    super.onPause();
+	 
+	    CCDirector.sharedDirector().pause();
+	}
+	 
+	@Override
+	public void onResume()
+	{
+	    super.onResume();
+	 
+	    CCDirector.sharedDirector().resume();
+	}
+	 
+	@Override
+	public void onStop()
+	{
+	    super.onStop();
+	 
+	    CCDirector.sharedDirector().end();
+	}
+}
